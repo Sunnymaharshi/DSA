16b4092092edda1eaad0afb95f4e29c52bc0533a
Tangasani Maharshi Reddy <61460595+Sunnymaharshi@users.noreply.github.com>
2022-03-07 17:37:14 +0530

    graphs problems rename

--- Graph Series/1-BFS.cpp
+++ Graph Series/1-BFS.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 void bfs(int n,int node,vector<int> &vis,vector<vector<int>> &adj, vector<int> &bfs_path){
     queue<int> q;
-    q.push(node);              // add into queue
-    vis[node]=1;               // mark as visited
+    q.push(node);               // add into queue
+    vis[node]=1;                // mark as visited
     while(!q.empty()){
         int cur_node=q.front();    
         q.pop();         
@@ -14,25 +14,25 @@ void bfs(int n,int node,vector<int> &vis,vector<vector<int>> &adj, vector<int> &
         for(int i=1;i<=n;i++){
             if(adj[cur_node][i]==1 && vis[i]!=1){   // add all its unvisited adjacent nodes into queue
                 q.push(i);
-                vis[i]=1; // mark as visited
+                vis[i]=1;       // mark as visited
             }
         }
     }
 }
 int main(){
-    int n,edges; //nodes,edges
+    int n,edges;                     // nodes,edges
     cin>>n>>edges;
     vector<vector<int>> adj(n+1,vector<int>(n+1,0));  // adjacency matrix
-    vector<int> vis(n+1,0); //visited array
+    vector<int> vis(n+1,0);         //visited array
     vector<int> bfs_path; 
     for(int i=0;i<edges;i++){
         int a,b;
         cin>>a>>b;
         adj[a][b]=1;
-        adj[b][a]=1;// comment this line for directed graph
+        adj[b][a]=1;                // comment this line for directed graph
     }
     for(int node=1;node<=n;node++){          // for checking all components
-        if(!vis[node]){                // process node only if it is not visited
+        if(!vis[node]){                     // process node only if it is not visited
             bfs(n,node,vis,adj,bfs_path);
         }
     }
